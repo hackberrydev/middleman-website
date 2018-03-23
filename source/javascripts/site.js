36816bc882991df86b3d41715e4b3abf0072d8b9
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2018-03-23 17:17:28 +0100

    Implement toggle next

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,15 @@
-// This is where it all goes :)
+window.Hackberry = {};
+
+Hackberry.ToggleNext = (function($) {
+  function init() {
+    $(document).on("click", "[data-toggle-next]", function() {
+      $(this).next().slideToggle();
+    });
+  }
+
+  return {
+    init: init
+  };
+}(jQuery));
+
+Hackberry.ToggleNext.init();
