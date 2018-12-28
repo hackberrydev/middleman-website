a77066c2e739276425e168ddbf8247e63c24869a
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2018-12-28 20:46:39 +0000

    Remove ToggleNext JavaScript

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,15 +1,1 @@
 window.Hackberry = {};
-
-Hackberry.ToggleNext = (function($) {
-  function init() {
-    $(document).on("click", "[data-toggle-next]", function() {
-      $(this).next().slideToggle();
-    });
-  }
-
-  return {
-    init: init
-  };
-}(jQuery));
-
-Hackberry.ToggleNext.init();
