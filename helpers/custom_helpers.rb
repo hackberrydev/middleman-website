e275187c5b789d00dea212aa811369b5416b7057
Nebojša Stričević <strika@hackberry.dev>
2022-06-16 05:40:08 +0000

    Extract code_block helper

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 module CustomHelpers
-    def contact_link(text)
+  def contact_link(text)
     link_to text, "mailto:strika@hackberry.dev"
+  end
+
+  def code_block(language, &block)
+    code = capture_html(&block)
+
+    highlighted_code = Middleman::Syntax::Highlighter.highlight(
+      code, language, {}
+    ).html_safe
+
+    concat_content content_tag(
+      :div, highlighted_code, :class => "not-prose my-5"
+    )
   end
 end
