cfa45dee5eaf1ab78df743e13bcdefc528e8b3ca
Nebojša Stričević <strika@hackberry.dev>
2022-07-01 05:32:50 +0000

    Add skeleton for plan example JS

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,14 @@
+window.Hackberry = {};
 
+Hackberry.PlanExample = (function() {
+  function init() {
+    document.querySelectorAll("[data-date-example]").forEach(function(el) {
+      console.log(el.getAttribute("data-date-example"));
+    });
+  }
+  return {
+    init: init
+  };
+})();
+
+Hackberry.PlanExample.init();
